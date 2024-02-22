9361239ffdf7edd635843043d2fa2685328a67be
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2024-02-22 09:49:09 +0100

    add projects page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		theme: {
-			fontSize: {
-				sm: '0.750rem',
-				base: '1rem',
-				xl: '1.333rem',
-				'2xl': '1.777rem',
-				'3xl': '2.369rem',
-				'4xl': '3.158rem',
-				'5xl': '4.210rem',
-			},
-			fontFamily: {
-				heading: 'Ubuntu',
-				body: 'Poppins',
-			},
-			fontWeight: {
-				normal: '400',
-				bold: '700',
-			},
-		}
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    theme: {
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Ubuntu',
+        body: 'Poppins',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
     themes: [
       {
         light: {
